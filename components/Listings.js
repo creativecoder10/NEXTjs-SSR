82ccee91072dc2a895e0e00f6549fb294aa4dd03
deepesh dang <dang.deepesh10@gmail.com>
2021-11-25 19:20:26 +1100

    Added the logic for making network call from Listings component from the custom hook

--- components/Listings.js
+++ components/Listings.js
@@ -1,28 +1,67 @@
+import { useEffect, useState } from "react";
+import useHttp from "../hooks/use-http";
+
 const ACTIONS = ["View", "Reply"];
 
-const formatAsCurrency = (int) => {
-    // TODO
-};
-
 const Listing = (props) => {
-    // TODO
-    // This should be the component which renders an individual listing to the page
-
-    return <div className="listing"></div>;
+  // TODO
+  // This should be the component which renders an individual listing to the page
+  /**
+   * Developer notes
+   * 1. I would have preferred to make it a separate component but with time
+   */
+  
 };
 
 const Listings = (props) => {
-    // TODO
-    // This component should make a request to the api endpoint (props.dataEndpoint)
-    // then render the result as set of listings as per the design mocks
-    // check props passed in from parent for other values that you may need to use
 
-    return (
-        <div>
-            <div className="listings__header"></div>
-            <div className="listings__grid"></div>
-        </div>
-    );
+    // Method to call to format the currency value into a currency data type 
+
+    const formatAsCurrency = (data) => {
+        // TODO
+        let transformedData = [...data];
+        transformedData.forEach((listing) => {
+          listing.price = Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "AUD",
+          }).format(listing.price);
+        });
+        setListings(transformedData);
+      };
+
+    //   listings is the local state, set to show the value of listings
+      const [listings, setListings]= useState([]);
+
+  // TODO
+  // This component should make a request to the api endpoint (props.dataEndpoint)
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchListings,
+  } = useHttp({ url: props.dataEndpoint }, formatAsCurrency);
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+  // then render the result as set of listings as per the design mocks
+  // check props passed in from parent for other values that you may need to use
+
+  return (
+    <div>
+      <div className="listings__header"></div>
+      <div className="listings__grid">
+      {
+          listings && listings.length > 0 && 
+          listings.map ((listing)=> {
+              return (
+                <Listing></Listing>
+              )
+          })
+      }
+      </div>
+    </div>
+  );
 };
 
 export default Listings;
